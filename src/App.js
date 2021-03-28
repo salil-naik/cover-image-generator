85206d794b1187df0650c26f788bc566b589d2f5
Salil Naik <salil.naik27@gmail.com>
2021-03-29 04:05:54 +0530

    add: meta data and other minor edits

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import html2canvas from "html2canvas";
 
 // components
 import { Cover } from "./components/Cover/index";
-import { Header } from "./components/Header/index";
 import { ColorBlock } from "./components/ColorBlock/index";
 
 function App() {
@@ -28,7 +27,7 @@ function App() {
       var img = canvas.toDataURL();
       let a = document.createElement("a");
       a.href = img;
-      a.download = `${name}'s-cover.png`;
+      a.download = `${name===""?'unknown':name}'s-cover.png`;
       a.click();
     });
   };
@@ -61,7 +60,19 @@ function App() {
   return (
     <>
       {/* FIXED ELEMENTS */}
-      {/* <Header /> */}
+      {/* github tag */}
+      <a href="https://github.com/salil-naik/cover-image-generator" target="_blank" rel="noreferrer">
+        <div className={style.ghTagContainer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className={style.ghSvg}
+          >
+            {" "}
+            <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z" />
+          </svg>
+        </div>
+      </a>
 
       {/* Cover Display */}
       <div className={style.coverWrapper}>
@@ -85,7 +96,15 @@ function App() {
       <footer className={style.footer}>
         <div className="grid-container">
           <div className={style.flex}>
-            <p>Cover Generator <span style={{fontSize: '12px', opacity: '0.7'}}>by <a href="" target="_blank">Salil Naik</a></span></p>
+            <p>
+              Cover Image Generator{" "}
+              <span style={{ fontSize: "12px", opacity: "0.7" }}>
+                by{" "}
+                <a href="https://github.com/salil-naik" target="_blank" rel="noreferrer">
+                  Salil Naik
+                </a>
+              </span>
+            </p>
             <div className={style.button} onClick={downloadCover}>
               Download Cover
             </div>
@@ -105,9 +124,9 @@ function App() {
                 ["headline", headline],
                 ["highlights", highlights],
                 ["tagline", tagline],
-              ].map((item) => {
+              ].map((item, index) => {
                 return (
-                  <div className="cell large-3">
+                  <div className="cell large-3 medium-6" key={index}>
                     <label htmlFor={item[0]} className={style.label}>
                       {item[0]}
                     </label>
@@ -124,18 +143,19 @@ function App() {
             </div>
           </form>
 
-          {/* Color selection */}
+          {/* Color selection section*/}
           <div className="grid-x grid-margin-x">
             <div className="cell large-6">
               <div className={color.container}>
                 <div className={color.railContainer}>
                   <div className={color.rail}>
-                    {colorArr.map((colors) => {
+                    {colorArr.map((colors, index) => {
                       return (
                         <div
                           onClick={() => {
                             changeColor(colors[0], colors[1]);
                           }}
+                          key={index}
                         >
                           <ColorBlock
                             color={{ bg: colors[0], border: colors[1] }}
